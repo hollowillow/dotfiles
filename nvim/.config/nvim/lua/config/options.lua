09d4be5467eeb7425d22f6e23b2bb026e7aea2b3
hollowillow <slavicekmichal@proton.me>
2024-12-21 17:05:56 +0100

    init commit

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.o.number = true
+vim.o.tabstop = 8
+vim.o.shiftwidth = 8
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
+vim.o.termguicolors = true
+vim.o.mouse = 'a'
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
