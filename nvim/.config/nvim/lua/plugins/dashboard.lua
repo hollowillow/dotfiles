032969f01f258bc7d5d95af7561fb4d1f09f38f4
hollowillow <slavicekmichal@proton.me>
2024-12-21 18:08:17 +0100

    added git gutter, lualine and visual-multi plugins

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -4,7 +4,7 @@ return {
         event = 'VimEnter',
         config = function()
                 require('dashboard').setup {
-                        -- config
+                -- config
                 }
         end,
         dependencies = { 'nvim-tree/nvim-web-devicons' },
