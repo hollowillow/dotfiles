d89ab640dc84bb5927694f1213bfc931a9273951
hollowillow <slavicekmichal@proton.me>
2024-12-21 18:40:20 +0100

    added keymaps for telescope and oil to keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,11 @@
-vim.keymap.set('i', 'ii', '<Esc>')
+-- Core
+vim.keymap.set('i', 'ii', '<Esc>', { desc = 'Exit visual mode by typing ii' })
+
+-- Oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Telescope
+vim.keymap.set('n', '<leader>ff', "<CMD>Telescope find_files<CR>", { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', "<CMD>Telescope live_grep<CR>", { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', "<CMD>Telescope buffers<CR>", { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', "<CMD>Telescope help_tags<CR>", { desc = 'Telescope help tags' })
