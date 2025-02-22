e738f8a8309e6c78010263ec59048a3fdcfe75fd
hollowillow <slavicekmichal@proton.me>
2025-02-22 10:15:33 +0100

    Add scripts, with keybind

---
+++ .bin/bm-yt.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+xdotool type $(
+        #removes blank and commented line, but keeps comments at
+        #the end of a line, for dmenu to display it properly
+        sed "/^#/ d; /^$/ d" $HOME/.config/newsboat/bookmarks |
+        dmenu -i -l 20 |
+        cut -d' ' -f1
+)
