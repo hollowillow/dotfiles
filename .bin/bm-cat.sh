f5fdec3c4839ae539e247a954e57f0e53cad0d9d
hollowillow <slavicekmichal@proton.me>
2025-03-16 13:46:13 +0100

    bookmark.sh script replaced bm-add, bm-cat, bm, and it's earlier version bm-test, added into sxhkd for keybind

--- .bin/bm-cat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xdotool type $(
-        cat $HOME/notes/bookmarks/youtube $HOME/notes/bookmarks/git $HOME/notes/bookmarks/other $HOME/.config/newsboat/urls |
-        sed "/^#/ d; /^$/ d; s|feeds/videos.xml?channel_id=|channel/|" |
-        dmenu -i -l 20 -p "Select bookmark: " |
-        cut -d' ' -f1
-)
