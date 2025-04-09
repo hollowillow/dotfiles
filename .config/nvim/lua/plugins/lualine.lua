4451cdf4d0693c7465998106ee66919f711dd913
hollowillow <slavicekmichal@proton.me>
2025-04-09 21:10:20 +0200

    added wordcount for markdown files

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,8 +7,17 @@ return {
                         lualine_a = {'mode'},
                         lualine_b = {'branch', 'diff', 'diagnostics'},
                         lualine_c = {'filename'},
-                        lualine_x = {'filetype', 'searchcount'},
-                        lualine_y = {'progress'},
+                        lualine_x = {'filetype', 'searchcount',},
+                        lualine_y = {'progress',
+                        -- show wordcount for markdown files
+                                function()
+                                        if vim.bo.filetype == 'markdown' then
+                                                return "Words: " .. vim.fn.wordcount().words
+                                        else
+                                                return ''
+                                end
+                        end
+                        },
                         lualine_z = {'location'}
                 },
                 extensions = {
