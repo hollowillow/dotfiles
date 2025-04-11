de1e1d5affe4faa1182ba72f5c61ac41dfb423fe
hollowillow <slavicekmichal@proton.me>
2025-04-11 11:45:14 +0200

    Add oil.lua plugin to neovim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -7,5 +7,13 @@ return {
         -- Optional dependencies
         -- dependencies = { { "echasnovski/mini.icons", opts = {}, }, },
         dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function ()
+                require('oil').setup({
+                        default_file_explorer = true,
+                        columns = { "icon" }, -- "permissions", "mtime", "size"
+                })
+                -- Keymaps
+                vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        end,
         },
 }
